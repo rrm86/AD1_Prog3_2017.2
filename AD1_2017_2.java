ca278685cd41d03c83d4b4c52f21653254fb0358
Ronnald R Machado <rrmachado@id.uff.br>
2017-08-29 20:08:06 -0300

    Versao Final

--- AD1_2017_2.java
+++ AD1_2017_2.java
@@ -7,8 +7,14 @@ public class AD1_2017_2 {
 		// TODO Auto-generated method stub
 		Prova superprova = new Prova(2);//Passando a quantidade de questões
 		/**
-		 * Questão C e D
-		 */
+		 * O enunciado da letra B não deixa claro o que deve ser feito caso o numero de questões
++		 * Seja Diferente de dois
++		 * Estou construindo o OBJ como pedido no enunciado porém o construtor da 
++		 * Classe prova não é capaz de manipular a criação de novas questões.
+		 *	
+ 		 * Caso fosse necessário criar uma questão "inédita" também seria preciso informar o tipo da questão.
+ 		 * Entretanto nada disso foi pedido no enunciado
+ 		 */
 		
 		superprova.imprimeProva();
 		
